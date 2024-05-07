517f50d345a6d617810d4127dfb5f094dd68bd90
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-07 17:00:09 +0200

    added the TConfig class and units to manage program settings (#17)

--- src/DProj2WinSetup.dpr
+++ src/DProj2WinSetup.dpr
@@ -7,7 +7,10 @@ uses
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
-  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule};
+  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule},
+  Olf.RTL.CryptDecrypt in '..\lib-externes\librairies\src\Olf.RTL.CryptDecrypt.pas',
+  Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
+  uConfig in 'uConfig.pas';
 
 {$R *.res}
 
---
+++ src/uConfig.pas
@@ -0,0 +1,134 @@
+unit uConfig;
+
+interface
+
+type
+  TConfig = class
+  private
+    class function GetExeBulkSigningServerIP: string; static;
+    class procedure SetExeBulkSigningServerIP(const Value: string); static;
+    class procedure SetExeBulkSigningServerPort(const Value: word); static;
+    class function GetExeBulkSigningServerPort: word; static;
+    class function GetExeBulkSigningAuthKey: string; static;
+    class procedure SetExeBulkSigningAuthKey(const Value: string); static;
+  protected
+  public
+    class property ExeBulkSigningServerIP: string read GetExeBulkSigningServerIP
+      write SetExeBulkSigningServerIP;
+    class property ExeBulkSigningServerPort: word
+      read GetExeBulkSigningServerPort write SetExeBulkSigningServerPort;
+    class property ExeBulkSigningAuthKey: string read GetExeBulkSigningAuthKey
+      write SetExeBulkSigningAuthKey;
+    class procedure Save;
+    class procedure Cancel;
+  end;
+
+implementation
+
+uses
+  System.Classes,
+  System.Types,
+  Olf.RTL.CryptDecrypt,
+  Olf.RTL.Params;
+
+var
+  ConfigFile: TParamsFile;
+
+procedure initConfig;
+begin
+  ConfigFile := TParamsFile.Create;
+  ConfigFile.InitDefaultFileNameV2('OlfSoftware', 'DProj2WinSetup', false);
+{$IFDEF RELEASE }
+  ConfigFile.onCryptProc := function(Const AParams: string): TStream
+    var
+      Keys: TByteDynArray;
+      ParStream: TStringStream;
+    begin
+      ParStream := TStringStream.Create(AParams);
+      try
+{$I '..\_PRIVATE\src\ConfigFileXORKey.inc'}
+        result := TOlfCryptDecrypt.XORCrypt(ParStream, Keys);
+      finally
+        ParStream.free;
+      end;
+    end;
+  ConfigFile.onDecryptProc := function(Const AStream: TStream): string
+    var
+      Keys: TByteDynArray;
+      Stream: TStream;
+      StringStream: TStringStream;
+    begin
+{$I '..\_PRIVATE\src\ConfigFileXORKey.inc'}
+      result := '';
+      Stream := TOlfCryptDecrypt.XORdeCrypt(AStream, Keys);
+      try
+        if assigned(Stream) and (Stream.Size > 0) then
+        begin
+          StringStream := TStringStream.Create;
+          try
+            Stream.Position := 0;
+            StringStream.CopyFrom(Stream);
+            result := StringStream.DataString;
+          finally
+            StringStream.free;
+          end;
+        end;
+      finally
+        Stream.free;
+      end;
+    end;
+{$ENDIF}
+  ConfigFile.load;
+end;
+
+{ TConfig }
+
+class procedure TConfig.Cancel;
+begin
+  ConfigFile.Cancel;
+end;
+
+class function TConfig.GetExeBulkSigningAuthKey: string;
+begin
+  result := ConfigFile.getValue('ESBAuthKey', '');
+end;
+
+class function TConfig.GetExeBulkSigningServerIP: string;
+begin
+  result := ConfigFile.getValue('ESBServerIP', '');
+end;
+
+class function TConfig.GetExeBulkSigningServerPort: word;
+begin
+  result := ConfigFile.getValue('ESBServerPort', 8080);
+end;
+
+class procedure TConfig.Save;
+begin
+  ConfigFile.Save;
+end;
+
+class procedure TConfig.SetExeBulkSigningAuthKey(const Value: string);
+begin
+  ConfigFile.setValue('ESBAuthKey', Value);
+end;
+
+class procedure TConfig.SetExeBulkSigningServerIP(const Value: string);
+begin
+  ConfigFile.setValue('ESBServerIP', Value);
+end;
+
+class procedure TConfig.SetExeBulkSigningServerPort(const Value: word);
+begin
+  ConfigFile.setValue('ESBServerPort', Value);
+end;
+
+initialization
+
+initConfig;
+
+finalization
+
+ConfigFile.free;
+
+end.
